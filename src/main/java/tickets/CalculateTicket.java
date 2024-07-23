ab17bc592e2517f39f776856625eeae0300c2954
bogin-kristina <Boginkristina@gmail.com>
2024-07-23 13:28:38 -0700

    Added unit tests for ticket price calculating program

---
+++ src/main/java/tickets/CalculateTicket.java
@@ -0,0 +1,25 @@
+package tickets;
+
+public class CalculateTicket {
+    private double basePrice;
+    private int age;
+
+
+    public CalculateTicket(double basePrice, int age) {
+        this.basePrice = basePrice;
+        this.age = age;
+    }
+
+    public double getPrice(){
+        if(age <= 2){
+            return 0;
+        }
+        else if(age <= 12){
+            return 0.5 * basePrice;
+        }
+        else if(age > 65){
+            return 0.8 * basePrice;
+        }
+        return basePrice;
+    }
+}
---
+++ src/test/java/unitTest/UnitTestTickets.java
@@ -0,0 +1,41 @@
+package unitTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tickets.CalculateTicket;
+
+public class UnitTestTickets {
+    @Test
+    public void calculatePrice_infant_freeTicket(){
+        CalculateTicket infant = new CalculateTicket(400, 1);
+        double price = infant.getPrice();
+        Assert.assertEquals(price, 0.0);
+    }
+
+    @Test
+    public void calculatePrice_child_50percentDiscountTicket(){
+        CalculateTicket child = new CalculateTicket(400, 8);
+        double price = child.getPrice();
+        Assert.assertEquals(price, 200.0);
+    }
+
+    @Test
+    public void calculatePrice_adult_fullPriceTicket(){
+        CalculateTicket child = new CalculateTicket(400, 48);
+        double price = child.getPrice();
+        Assert.assertEquals(price, 400.0);
+    }
+
+    @Test
+    public void calculatePrice_senior_20percentDiscountTicket(){
+        CalculateTicket child = new CalculateTicket(400, 73);
+        double price = child.getPrice();
+        Assert.assertEquals(price, 320.0);
+    }
+
+}
+
+
+
+
+
